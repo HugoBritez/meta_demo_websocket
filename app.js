7da177e086487ab832bf4e8fd4adcad6b7692bd7
HugoBritez <sebastianbritezdevelop@gmail.com>
2025-07-26 10:29:51 -0300

    fixes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-const verifyToken = process.env.VERIFYTOKEN;
+const verifyToken = process.env.VERIFY_TOKEN;
 
 app.get('/', (req, res)=> {
   const { 'hub.mode': mode, 'hub.challenge': challenge, 'hub.verify_token': token} = req.query;
